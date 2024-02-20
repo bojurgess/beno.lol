5742ccbbd13e38499ad7409c24925d3cc80a187e
Joe Burgess <bojurgess@gmail.com>
2024-02-20 20:44:25 +0000

    Consolidate env

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -4,9 +4,7 @@ import { DatabaseError } from "@planetscale/database";
 import * as schema from "./schema";
 
 const connection = connect({
-	host: process.env["DATABASE_HOST"] as string,
-	username: process.env["DATABASE_USERNAME"] as string,
-	password: process.env["DATABASE_PASSWORD"] as string,
+	url: process.env["DATABASE_URL"],
 });
 
 export const db = drizzle(connection, { schema });
